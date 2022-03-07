77134c7fd86410588020f9045e8f04909cbf3dd8
변지은 <bje5774@gmail.com>
2022-03-07 17:14:07 +0900

    edoc1week

--- edoc1week/src/edoc1week/b11582.java
+++ edoc1week/src/edoc1week/b11582.java
@@ -8,19 +8,24 @@ public class b11582 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc= new Scanner(System.in);
-		int store_num= sc.nextInt(); //// 치킨집 가게 수 입력
+		int store_num= sc.nextInt(); // 치킨집 가게 수 입력
 		int arr[] = new int [store_num];  // 치킨집 맛 수치 저장 배열 선언, 생성
 		for(int i=0; i<store_num; i++) { //치킨집 맛 수치 저장
 			arr[i]= sc.nextInt();
 		}	
-		int stop= sc.nextInt();//몇 번째에 멈출건지
+		int stop= sc.nextInt();//몇 번째에 멈출건지, 최대는 (store_num/2-1)번
 		int first_index= 1;// 정렬 인덱스 시작점 
 		int k=0;
-		for(int i=0; i<stop; i++) {
-			//ex) 치킨집이 8개이고 첫번째에 멈춘다면 정렬해야 될 인덱스 처음 번호는 0 2 4 6 이다. 따라서 첫번째에 멈춘다면 2의 1승 만큼 인덱스가 더해지고
-			// 두번째에 멈춘다면 2의 2승만큼 인덱스가 더해지므로 처음 인덱스를 몇씩 더해야할지 구하는 for loop.
+		for(int i=0; i<stop; i++) {//몇 번째 정렬에 멈출건
+		//ex) 치킨집이 8개이고 첫번째에 멈춘다면 정렬해야 될 인덱스 처음 번호는 0 2 4 6 이다. 따라서 첫번째에 멈춘다면 2의 1승 만큼 인덱스가 더해지고
+		// 두번째에 멈춘다면 2의 2승만큼 인덱스가 더해지므로 처음 인덱스를 몇씩 더해야할지 구하는 for loop.
+			if(first_index>=store_num) {
+				first_index= first_index/2;
+			}
+			
 			first_index= first_index*2;
-		}
+			
+			}
 
 			for(int j=0; j<store_num/first_index ; j++) {
 				// 정렬해야 되는 횟수, 예를 들어 배열 8개 짜리 정렬을 첫번째에 컷해서 구한다면 2개씩 짝 지어서 4번 정렬해야됨
--- edoc1week/src/edoc1week/b9009.java
+++ edoc1week/src/edoc1week/b9009.java
@@ -13,7 +13,8 @@ public class b9009 {
 		for(int i =0; i<testarr.length; i++){//출력할 데이터 배열 입력
 			testarr[i] = sc.nextInt();
 		}
-		int[] fibo = new int[45];//피보나치 배열 선언 생성, 1 ≤ n ≤ 1,000,000,000 이므로 45번째 피보나치 수는 1,134,903,170 이므로 배열은 45개만 생성 
+		int[] fibo = new int[45];
+		//피보나치 배열 선언 생성, 1 ≤ n ≤ 1,000,000,000 이므로 45번째 피보나치 수는 1,134,903,170 이므로 배열은 45개만 생성 
 		fibo[0] = 1;//fibo[0]=0은 건너뛴다.
 		fibo[1] = 1;
 		for(int i=2; i<fibo.length; i++){//피보나치 배열 입력
@@ -21,11 +22,12 @@ public class b9009 {
 		}
 		
 		Vector<Integer> print= new Vector<>();//출력할 벡터 생성(작은 수 부터 출력해야 하므로)
-		for(int i=0; i<num; i++) {//num만큼(4) 루프 돌리기
+		for(int i=0; i<num; i++) {//num만큼(예시에서는 4) 루프 돌리기
 			int t= testarr[i];//구해야되는 수 복제, 빼야 되는데 test[i]에 바로하면 값이 누적 되므로 하나 복제한다.
 			
 			
-			for(int j=44; j<fibo.length;j--) {//큰 수부터 구해야되므로 피보나치 배열 뒤에서부터 시작한다.
+			for(int j=44; j<fibo.length;j--) {
+				//최소 갯수의 피보나치 수들의 합으로 나타내기 때문에 큰 수부터 구해야되므로 피보나치 배열을 뒤에서부터 비교한다.
 				if(t==0) {//t가 0이 될 떄 break!
 					for(int k=print.size(); k>0; k--) {//저장한 벡터  출력, 뒤에서부터 출력
 						System.out.print(print.get(k-1)+" ");
