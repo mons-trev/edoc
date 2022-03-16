988ea42822d1d078af09dd3f4a21df49b0ff7ee0
변지은 <bje5774@gmail.com>
2022-03-16 17:24:45 +0900

    edoc7week-epper

---
+++ edoc7week/src/edoc7week/b2011.java
@@ -0,0 +1,50 @@
+package edoc7week;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b2011 {
+	public static final int MOD=1000000;
+	
+	public static int solution(String code, int n) {
+		int answer=0;
+		int [] sample= new int [code.length()];
+		int [] dp= new int[code.length()];
+		for(int i=0; i<sample.length; i++) {
+			sample[i]= Character.getNumericValue(code.charAt(i));
+			dp[i]=0;
+		}
+		if(sample[0]==0) return 0;
+		else {
+			dp[0]=1;
+		}
+		for(int i=1; i<n; i++) {
+			if(sample[i]==0) {//0ÀÏ ‹š
+				if(sample[i-1]==0 || sample[i-1]>2) break;
+				else {
+					if(i==1) dp[i]=1;
+					else dp[i]=dp[i-1]+dp[i-2];
+				}
+			}
+			else {
+				if((sample[i-1]>2)||(sample[i-1]==2&&sample[i]>6)) dp[i]=dp[i-1];
+				else {
+					if(i==1) dp[i]=2;
+					else dp[i]=dp[i-1]+dp[i-2];
+				}
+			}
+			dp[i]%=MOD;
+		}
+		
+		answer= dp[n-1];
+		return answer;
+	}
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str= br.readLine();
+		int answer = solution(str,str.length());
+		System.out.println(answer);
+	}
+
+}
