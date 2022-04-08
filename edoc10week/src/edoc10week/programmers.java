ec8013f39a95d87b510b9c8609b7d6e540bb0f9f
변지은 <bje5774@gmail.com>
2022-04-08 17:41:20 +0900

    edoc10weekprogrammers

--- edoc10week/src/edoc10week/programmers.java
+++ edoc10week/src/edoc10week/programmers.java
@@ -6,22 +6,30 @@ public class programmers {
 	    public static int solution(int n) {
 	        int answer = 0;
 	        
-	        for(int i=2; i<=n; i++) {
-	            boolean flag=true;
-	                for(int j=2; j<i; j++) {
-	                    if(i%j==0&&i!=1){
-	                     flag=false;
-	                     break;
-	                 }
-	            }
-	                if(flag==true) answer++;
+	        boolean [] prime = new boolean [n+1];
+	        
+	        for(int i=2; i<n+1; i++) {
+	        	prime[i]=true;
+	        }
+	        for(int i=2; i<n+1; i++) {
+	        	if(prime[i]==true) {
+	        		for(int j=2; i*j<n+1; j++) {
+	        			prime[i*j]=false;
+	        		}
+	        	}
+	        }
+	        for(int i=2; i<n+1; i++) {
+	        	if(prime[i]==true) {
+	        		answer++;
+	        		System.out.println(i);
+	        	}
 	        }
 	        return answer;
 	    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution(10));
+		System.out.println(solution(8));
 	}
 
 }
